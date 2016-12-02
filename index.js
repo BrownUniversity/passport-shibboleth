14ced0f2d89356e0cb2fa88be5322d15d91031f8
Sumner Warren <sumner@Sumners-MacBook-Pro.local>
2016-12-02 10:56:45 -0500

    Added private key argument, better metadata generation support, and better url parsing.

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
-var passport = require('passport'),
+var fs = require('fs'),
+    passport = require('passport'),
     samlStrategy = require('passport-saml').Strategy,
-    config = require('./config');
+    config = require('./config'),
+    url = require('url');
 
-module.exports = function (host, cbPath, issuer, attributeMap) {
-    if (typeof issuer === 'object' && issuer !== null) {
-        attributeMap = issuer;
-        issuer = null;
+module.exports = function (host, cbPath, privateKeyPath, attributeMap, issuer) {
+    // Recommend/ensure private key file
+    if (typeof privateKeyPath === 'undefined') {
+        console.log('Warning: A RSA private key is recommended for this package.');
     }
-    var defaultAttributes = {
+    if (typeof privateKeyPath === 'string' && ! fs.existsSync(privateKeyPath)) {
+        throw new Error('Private key file does not exist: ' + privateKeyPath);
+    }
+
+    // Handle missing attributeMap
+    if (typeof attributeMap === 'string' && typeof issuer === 'undefined') {
+        issuer = attributeMap;
+        attributeMap = {};
+    }
+
+    // Merge attributeMap with defaults
+    if (typeof attributeMap !== 'object') {
+        attributeMap = {};
+    }
+    attributeMap = Object.assign({
         'urn:oid:1.3.6.1.4.1.6537.1.19': 'uuid',
         'urn:oid:2.16.840.1.113730.3.1.241': 'displayName',
         'urn:oid:2.5.4.42': 'firstName',
@@ -25,44 +41,47 @@ module.exports = function (host, cbPath, issuer, attributeMap) {
         'urn:oid:1.3.6.1.4.1.5923.1.1.1.1': 'affiliations',
         'urn:oid:1.3.6.1.4.1.6537.1.25': 'status',
         'urn:oid:1.3.6.1.4.1.5923.1.5.1.1': 'isMemberOf'
-    };
-    if (attributeMap !== false) {
-        attributeMap = attributeMap || defaultAttributes;
-    }
+    }, attributeMap);
 
+    // Basically no-ops
     passport.serializeUser(function (user, done) {
         done(null, user);
     });
     passport.deserializeUser(function (user, done) {
         done(null, user);
     });
-    passport.use(new samlStrategy(config(host, cbPath, issuer),
+
+    // Handles mapping attributes
+    var strategy = new samlStrategy(
+        config(host, cbPath, privateKeyPath),
         function (profile, done) {
             var prof = {};
-            if (attributeMap) {
-                for (var a in attributeMap) {
-                    if (a in profile) {
-                        profile[attributeMap[a]] = prof[attributeMap[a]] = profile[a];
-                        delete profile[a];
-                    }
+
+            for (var a in attributeMap) {
+                if (a in profile) {
+                    prof[attributeMap[a]] = profile[a];
                 }
-            } else {
-                prof = profile;
             }
+
             return done(null, prof);
-        })
+        }
     );
+    passport.use(strategy);
+
     passport.logout = function (strategy, options) {
         options = options || {};
 
         return function (req, res) {
-            if (!options.successRedirect) {
-                options.successRedirect = host + '/';
-            }
-
+            var parsed = url.parse(options.successRedirect || '/');
+            var redirect = (parsed.host ? parsed.protocol + '//' + parsed.host : host) + parsed.path;
             req.logout();
-            return res.redirect('https://sso.brown.edu/idp/shib_logout.jsp?return=' + encodeURI(options.successRedirect));
+            return res.redirect('https://sso.brown.edu/idp/shib_logout.jsp?return=' + encodeURI(redirect));
         };
     };
-    return {'passport': passport, 'strategy': 'saml'};
+
+    return {
+      'passport': passport,
+      'strategy': 'saml',
+      'generateServiceProviderMetadata': strategy.generateServiceProviderMetadata.bind(strategy)
+    };
 };
