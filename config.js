6384f3702b23b5e49ca7732e7691f87edfb07010
Sumner Warren <sumner_warren@brown.edu>
2018-11-30 16:03:59 -0500

    Add eslint and prettier

--- config.js
+++ config.js
@@ -1,20 +1,27 @@
-var path = require('path');
-var fs = require('fs');
-var url = require('url');
-var assets = path.join(path.dirname(fs.realpathSync(__filename)), './assets');
+var fs = require("fs");
+var url = require("url");
 
-module.exports = function (host, cbPath, privateKeyPath, issuer) {
-    var parsed = url.parse(host);
-    var cbURL = parsed.protocol + '//' + parsed.host + (cbPath || '/login/callback').replace(/^\/*/, '/');
-    issuer = issuer || (parsed.protocol + '//' + parsed.hostname + '/shibboleth-sp');
-    return {
-        'callbackUrl': cbURL,
-        'entryPoint': 'https://sso.brown.edu/idp/profile/SAML2/Redirect/SSO',
-        'issuer': issuer,
-        'cert': 'MIIDHzCCAgegAwIBAgIUHPSdb4ae0QA3fyeEIe7wKXi2oT0wDQYJKoZIhvcNAQEFBQAwGDEWMBQGA1UEAxMNc3NvLmJyb3duLmVkdTAeFw0xMDA1MjEyMTAyMzFaFw0zMDA1MjEyMTAyMzFaMBgxFjAUBgNVBAMTDXNzby5icm93bi5lZHUwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQCxabgPhLgyIOyoG87T41EXwUOyXKNJ4XvFa5moC3y8J9JzWBeVRQ8L7zxEiLcsZ4qHYdOCVZNSD5SYeI8rtjwz/lHlJsF7dfLXpeQSm5HggcgWEEbvgp+qHRsbu6ZLKUmD0qLnWLZ5pG+ihm0n10G6h9op9NKJqJOgkyk3PQbe1biAjjpnkVL5CxL4h1E0XADZogZBh3pY04kNzbMvaWsB9ToDI8i6JqTfyEZZcQr4ShgIdstAHzbHcGoRqSOuI3BDeYDj0lp7/X/v/lvQMWWnKFJD6E7xKypoVO2czCswZ9i390YkI9+eoBbIMunuE/JjPZwwknnWqaI5LBDkGbnJAgMBAAGjYTBfMD4GA1UdEQQ3MDWCDXNzby5icm93bi5lZHWGJGh0dHBzOi8vc3NvLmJyb3duLmVkdS9pZHAvc2hpYmJvbGV0aDAdBgNVHQ4EFgQU1EgYrxblcWF0RP8BxGEPTM4SkIowDQYJKoZIhvcNAQEFBQADggEBAG1o9TLKHLDMlW+fRtyWXeGI1Dpnvw505nmWOQEPzXnT3oMRsmFYUrynl6amaay0IlEZ86g8KALM0AsKA9x2hYz/Kvs99ZLJJ+3mcHDswWWC9NYme6HEzs+mXRXD2wxOWUYNKc2xAs3QPOpYmX2g6sZiDPkyQ4KOZT/Vh9BZ970k7vtJ0lIOUFSDTQnlfGOtdEBE6QehIzHF6SpFakYYFyAK1MO4G/vdYYoe4lF5FTUg9UjnEWqwcmCX02ay5ma5YMkNkyXeetU6HmLRBnpwPcuCgIkzuXP64h+6nVWpbLEJHeHIom+fbTMoCnUDbZ0FcmiGxk9Cg6RPORcGztgLcVw=',
-        'decryptionPvk': typeof privateKeyPath === 'string' ? fs.readFileSync(privateKeyPath) : null,
-        'identifierFormat': 'urn:oasis:names:tc:SAML:1.1:nameid-format:unspecified',
-        'disableRequestedAuthnContext': true,
-        'acceptedClockSkewMs': 180000
-    };
+module.exports = function(host, cbPath, privateKeyPath, issuer) {
+  var parsed = url.parse(host);
+  var cbURL =
+    parsed.protocol +
+    "//" +
+    parsed.host +
+    (cbPath || "/login/callback").replace(/^\/*/, "/");
+  issuer =
+    issuer || parsed.protocol + "//" + parsed.hostname + "/shibboleth-sp";
+  return {
+    callbackUrl: cbURL,
+    entryPoint: "https://sso.brown.edu/idp/profile/SAML2/Redirect/SSO",
+    issuer: issuer,
+    cert:
+      "MIIDHzCCAgegAwIBAgIUHPSdb4ae0QA3fyeEIe7wKXi2oT0wDQYJKoZIhvcNAQEFBQAwGDEWMBQGA1UEAxMNc3NvLmJyb3duLmVkdTAeFw0xMDA1MjEyMTAyMzFaFw0zMDA1MjEyMTAyMzFaMBgxFjAUBgNVBAMTDXNzby5icm93bi5lZHUwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQCxabgPhLgyIOyoG87T41EXwUOyXKNJ4XvFa5moC3y8J9JzWBeVRQ8L7zxEiLcsZ4qHYdOCVZNSD5SYeI8rtjwz/lHlJsF7dfLXpeQSm5HggcgWEEbvgp+qHRsbu6ZLKUmD0qLnWLZ5pG+ihm0n10G6h9op9NKJqJOgkyk3PQbe1biAjjpnkVL5CxL4h1E0XADZogZBh3pY04kNzbMvaWsB9ToDI8i6JqTfyEZZcQr4ShgIdstAHzbHcGoRqSOuI3BDeYDj0lp7/X/v/lvQMWWnKFJD6E7xKypoVO2czCswZ9i390YkI9+eoBbIMunuE/JjPZwwknnWqaI5LBDkGbnJAgMBAAGjYTBfMD4GA1UdEQQ3MDWCDXNzby5icm93bi5lZHWGJGh0dHBzOi8vc3NvLmJyb3duLmVkdS9pZHAvc2hpYmJvbGV0aDAdBgNVHQ4EFgQU1EgYrxblcWF0RP8BxGEPTM4SkIowDQYJKoZIhvcNAQEFBQADggEBAG1o9TLKHLDMlW+fRtyWXeGI1Dpnvw505nmWOQEPzXnT3oMRsmFYUrynl6amaay0IlEZ86g8KALM0AsKA9x2hYz/Kvs99ZLJJ+3mcHDswWWC9NYme6HEzs+mXRXD2wxOWUYNKc2xAs3QPOpYmX2g6sZiDPkyQ4KOZT/Vh9BZ970k7vtJ0lIOUFSDTQnlfGOtdEBE6QehIzHF6SpFakYYFyAK1MO4G/vdYYoe4lF5FTUg9UjnEWqwcmCX02ay5ma5YMkNkyXeetU6HmLRBnpwPcuCgIkzuXP64h+6nVWpbLEJHeHIom+fbTMoCnUDbZ0FcmiGxk9Cg6RPORcGztgLcVw=",
+    decryptionPvk:
+      typeof privateKeyPath === "string"
+        ? fs.readFileSync(privateKeyPath)
+        : null,
+    identifierFormat: "urn:oasis:names:tc:SAML:1.1:nameid-format:unspecified",
+    disableRequestedAuthnContext: true,
+    acceptedClockSkewMs: 180000
+  };
 };
--- example/routes/index.js
+++ example/routes/index.js
@@ -1,73 +1,88 @@
-var express = require('express');
-var util = require('util');
-var url = require('url');
+var express = require("express");
+var url = require("url");
 var router = express.Router();
 
-module.exports = function (brownShib) {
-    /* GET home page. */
-    router.get('/', function (req, res, next) {
-        res.render('index', {
-            title: 'Brown Shib Example',
-            user: req.isAuthenticated() ? req.user : null
-        });
+module.exports = function(brownShib) {
+  /* GET home page. */
+  router.get("/", function(req, res) {
+    res.render("index", {
+      title: "Brown Shib Example",
+      user: req.isAuthenticated() ? req.user : null
     });
+  });
 
-		// Redirect the user to the Shibboleth IdP
-    router.get('/login', brownShib.authenticate());
+  // Redirect the user to the Shibboleth IdP
+  router.get("/login", brownShib.authenticate());
 
-    // Handle the Shibboleth POST
-    router.post('/login/callback', brownShib.authenticate({successRedirect: '/', failureRedirect: '/error'}));
+  // Handle the Shibboleth POST
+  router.post(
+    "/login/callback",
+    brownShib.authenticate({ successRedirect: "/", failureRedirect: "/error" })
+  );
 
-    // Render the user's profile or start auth flow if user is not authenticated.
-    // res.user contains the user information returned by Shibboleth
-    router.get('/profile', function (req, res) {
-        if (req.isAuthenticated()) {
-            res.render('profile', {user: req.user});
-        } else {
-            res.redirect('/login');
-        }
+  // Render the user's profile or start auth flow if user is not authenticated.
+  // res.user contains the user information returned by Shibboleth
+  router.get("/profile", function(req, res) {
+    if (req.isAuthenticated()) {
+      res.render("profile", { user: req.user });
+    } else {
+      res.redirect("/login");
+    }
+  });
+
+  /**
+   * brownShib.passport.logout does three things:
+   *  - Kills the local session (req.logout())
+   *  - Redirects to the Shibboleth IdP to kill its session (https://sso.brown.edu/idp/shib_logout.jsp)
+   *  - Provides a return url to the IdP to control where it redirects to (?return=<URL>).
+   *    If not provided, this defaults to the baseUrl of the request.
+   *
+   * You can set the return url by passing an options object with a successRedirect parameter.
+   * Alternatively, if you need complete control over the logout functionality,
+   * you can write a custom logout responder. Just don't forget to kill both
+   * the local and IdP sessions. Example below.
+   */
+  router.get("/logoutWithDefaultRedirect", brownShib.logout());
+
+  router.get(
+    "/logoutWithExplicitRedirect",
+    brownShib.logout({ successRedirect: "https://brown.edu" })
+  );
+
+  router.get(
+    "/logoutWithBeforeMiddleware",
+    function(req, res, next) {
+      // Do something here before the session is killed
+      // (ex. log last ativity time)
+      var activity = new Date();
+      // eslint-disable-next-line no-console
+      console.log(
+        req.user.firstName + " logged out at: " + activity.toString()
+      );
+
+      // Then call next()
+      next();
+    },
+    brownShib.logout()
+  );
+
+  router.get("/customLogout", function(req, res) {
+    // Kill local session
+    req.logout();
+
+    // Do other, custom things here
+
+    // Kill IdP session and redirect to index page
+    var redirect = url.format({
+      protocol: req.protocol,
+      host: req.get("Host"),
+      pathname: "/"
     });
+    res.redirect(
+      "https://sso.brown.edu/idp/shib_logout.jsp?return=" +
+        encodeURIComponent(redirect)
+    );
+  });
 
-    /**
-     * brownShib.passport.logout does three things:
-     *  - Kills the local session (req.logout())
-     *  - Redirects to the Shibboleth IdP to kill its session (https://sso.brown.edu/idp/shib_logout.jsp)
-     *  - Provides a return url to the IdP to control where it redirects to (?return=<URL>).
-     *    If not provided, this defaults to the baseUrl of the request.
-     *
-     * You can set the return url by passing an options object with a successRedirect parameter.
-     * Alternatively, if you need complete control over the logout functionality,
-     * you can write a custom logout responder. Just don't forget to kill both
-     * the local and IdP sessions. Example below.
-     */
-    router.get('/logoutWithDefaultRedirect', brownShib.logout());
-
-    router.get('/logoutWithExplicitRedirect', brownShib.logout({ successRedirect: 'https://brown.edu' }));
-
-    router.get('/logoutWithBeforeMiddleware', function(req, res, next) {
-        // Do something here before the session is killed
-        // (ex. log last ativity time)
-        var activity = new Date();
-        console.log(req.user.firstName + ' logged out at: ' + activity.toString());
-
-        // Then call next()
-        next();
-    }, brownShib.logout());
-
-    router.get('/customLogout', function(req, res) {
-        // Kill local session
-        req.logout();
-    
-        // Do other, custom things here
-    
-        // Kill IdP session and redirect to index page
-        var redirect = url.format({
-          protocol: req.protocol,
-          host: req.get('Host'),
-          pathname: '/'
-        });
-        res.redirect('https://sso.brown.edu/idp/shib_logout.jsp?return=' + encodeURIComponent(redirect));
-    });
-
-    return router;
-}
+  return router;
+};
